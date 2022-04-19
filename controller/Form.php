81461946e448a5697facbdfcd78eb8817a03f9e5
TobiasMaugus <tobiasmaugus705@gmail.com>
2022-04-19 08:35:25 -0300

    aula 19/04 commit2

--- controller/Form.php
+++ controller/Form.php
@@ -4,8 +4,13 @@ class Form
   private $message = "";
   public function controller()
   {
-    $this->message = "Estou na classe Form";
+    $form = new Template("view/form.html");
+    $this->message = $form->saida();
   }
+  public function salvar(){
+    print_r($_POST);
+  }
+
   public function getMessage()
   {
     return $this->message;
--- model/Connection.php
+++ model/Connection.php
@@ -1,3 +1,4 @@
+<?php
 final class Connection{
     private function __construct(){
     }    
