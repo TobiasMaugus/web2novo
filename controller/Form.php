64c6eea3aeda169f1e007895973917d3dd22495c
TobiasMaugus <tobiasmaugus705@gmail.com>
2022-05-03 08:17:00 -0300

    tudo pronto menos o css

--- controller/Form.php
+++ controller/Form.php
@@ -2,11 +2,11 @@
 class Form
 {
   private $message = "";
+  private $error = "";
   public function __construct()
   {
     Transaction::open();
   }
-    
   public function controller()
   {
     $form = new Template("view/form.html");
@@ -16,9 +16,10 @@ class Form
     $form->set("preco", "");
     $this->message = $form->saida();
   }
-  public function salvar(){
-    if(isset($_POST["prato"]) && isset($_POST["ingredientes"]) && isset($_POST["preco"])){
-      try{
+  public function salvar()
+  {
+    if (isset($_POST["prato"]) && isset($_POST["ingredientes"]) && isset($_POST["preco"])) {
+      try {
         $conexao = Transaction::get();
         $cardapio = new Crud("cardapio");
         $prato = $conexao->quote($_POST["prato"]);
@@ -36,12 +37,17 @@ class Form
             "id = $id"
           );
         }
+        $this->message = $cardapio->getMessage();
+        $this->error = $cardapio->getError();
       } catch (Exception $e) {
-        echo $e->getMessage();
+        $this->message = $e->getMessage();
+        $this->error = true;
       }
+    } else {
+      $this->message = "Campos não informados!";
+      $this->error = true;
     }
   }
-
   public function editar()
   {
     if (isset($_GET["id"])) {
@@ -50,22 +56,38 @@ class Form
         $id = $conexao->quote($_GET["id"]);
         $cardapio = new Crud("cardapio");
         $resultado = $cardapio->select("*", "id = $id");
-        $form = new Template("view/form.html");
-        foreach ($resultado[0] as $cod => $valor) {
-          $form->set($cod, $valor);
-        } 
-        $this->message = $form->saida();
+        if (!$cardapio->getError()) {
+          $form = new Template("view/form.html");
+          foreach ($resultado[0] as $cod => $valor) {
+            $form->set($cod, $valor);
+          }
+          $this->message = $form->saida();
+        } else {
+          $this->message = $cardapio->getMessage();
+          $this->error = true;
+        }
       } catch (Exception $e) {
-        echo $e->getMessage();
+        $this->message = $e->getMessage();
+        $this->error = true;
       }
     }
   }
-
   public function getMessage()
   {
-    return $this->message;
+    if (is_string($this->error)) {
+      return $this->message;
+    } else {
+      $msg = new Template("view/msg.html");
+      if ($this->error) {
+        $msg->set("cor", "danger");
+      } else {
+        $msg->set("cor", "success");
+      }
+      $msg->set("msg", $this->message);
+      $msg->set("uri", "?class=Tabela");
+      return $msg->saida();
+    }
   }
-
   public function __destruct()
   {
     Transaction::close();
